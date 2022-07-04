4100c744e90230bd9fb26f6d18afe90b654f232c
arunima297 <99283459+arunima297@users.noreply.github.com>
2022-07-03 22:39:27 -0400

    Delete modules directory

--- modules/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vnet" {
-  value = azurerm_virtual_network.vnet_3385
-}
-
-output "subnet" {
-  value = azurerm_subnet.subnet_3385
-}
--- modules/resource_group/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "resource_group_3385" {
-    name = var.resource_group
-    location = var.location
-    tags = local.common_tags
-}
-
-
-
