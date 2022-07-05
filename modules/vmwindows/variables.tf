ef6f94ab201a23cccf75a525e7b8c644c40c3ab3
arunima297 <99283459+arunima297@users.noreply.github.com>
2022-07-05 19:34:28 -0400

    Update variables.tf

--- modules/vmwindows/variables.tf
+++ modules/vmwindows/variables.tf
@@ -24,41 +24,7 @@ variable "windows_name" {
   default = ""
 }
 
-variable "windows_size" {
-  default = "Standard_B1ms"
-}
-
-variable "admin_username" {
-  default = "n01523385"
-}
-
-variable "admin_password" {
-  default = "n01523385@arunima"
-}
-
-variable windows_os_disk {
-    type = map(string)
-    default = {
-        create_option = "Attach"
-        storage_account_type = "StandardSSD_LRS"
-        disk_size = 128
-        caching = "ReadWrite"
-    }
-}
-
-variable "windows_avs" {
-    default = "windows_avs"
-}
 variable "storage_account_uri" {
     default = ""
-}
-
-variable "windows_vmexe" {
-  type = map(string)
-  default = {
-    publisher  = "Microsoft.Azure.Security.AntimalwareSignature"
-    type  = "AntimalwareConfiguration"
-    type_handler_version ="2.58"
-  }
 }
 
